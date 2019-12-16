688008b52973e16666689aa4b60eab26177d61ff
Rachel Perez <reperez@me.com>
2019-12-16 00:26:26 -0500

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'aws-server',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'http://ec2-18-188-229-1.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/express.pem',
+      ref: 'origin/master',
+      repo: 'https://github.com/reperez120/aws-server.git',
+      path: '/home/ubuntu/aws-server',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
